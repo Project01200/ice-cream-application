63f29e0fb4e197ff63014fe6978afa05ac789a36
KavyaKum <kavya01kumar@gmail.com>
2024-06-18 15:05:46 +0530

    xyz

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import IceCreamFlavours, Toppings, Containers, Size, Ingredients, MyOrders
+from .models import IceCreamFlavours, Toppings, Containers, Size, MyOrders, Toppings_Ingredients, Flavors_Ingredients, \
+    Containers_Ingredients
 
 admin.site.register(IceCreamFlavours)
 admin.site.register(Toppings)
 admin.site.register(Containers)
 admin.site.register(Size)
-admin.site.register(Ingredients)
+admin.site.register(Flavors_Ingredients)
+admin.site.register(Toppings_Ingredients)
+admin.site.register(Containers_Ingredients)
 admin.site.register(MyOrders)
--- app/models.py
+++ app/models.py
@@ -2,33 +2,6 @@
 
 from django.db import models
 
-
-class MyOrders(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField()
-    items = models.CharField(max_length=255)
-    quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    phone_num = models.CharField(max_length=15)
-
-    def __str__(self):
-        return f"Order {self.pk} by {self.name}"
-
-
-# class Orders(models.Model):
-#     name = models.CharField(max_length=50)
-#     email = models.EmailField()
-#     items = models.CharField(max_length=255)
-#     address = models.TextField()
-#     quantity = models.IntegerField()
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-#     phone_num = models.CharField(max_length=15)
-#     date_ordered = models.DateTimeField(auto_now_add=True)
-#
-#     def __str__(self):
-#         return f"Order {self.id} by {self.name}"
-
-
 class IceCreamFlavours(models.Model):
     name = models.CharField(max_length=20)
     price = models.IntegerField()
@@ -52,19 +25,68 @@ class Containers(models.Model):
     def __str__(self):
         return self.container_name
 
-
-class Ingredients(models.Model):
-    flavour = models.ForeignKey(IceCreamFlavours, on_delete=models.CASCADE)
-    flavour_ingredients = models.TextField()
-
+class MyOrders(models.Model):
+    name = models.CharField(max_length=50)
+    flavor = models.ForeignKey(IceCreamFlavours, on_delete=models.CASCADE)
+    topping = models.ForeignKey(Toppings, on_delete=models.CASCADE)
     container = models.ForeignKey(Containers, on_delete=models.CASCADE)
-    container_ingredients = models.TextField()
-
-    toppings = models.ForeignKey(Toppings, on_delete=models.CASCADE)
-    toppings_ingredients = models.TextField()
+    email = models.EmailField()
+    items = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"Ingredients for {self.flavour}"
+        return f"Order {self.pk} by {self.name}"
+
+class Containers_Ingredients(models.Model):
+    container = models.ForeignKey(Containers, on_delete=models.CASCADE)
+    container_ingredients = models.CharField(max_length=40, null=False, blank=False)
+
+    def __str__(self):
+        return self.container_ingredients
+
+class Toppings_Ingredients(models.Model):
+    topping = models.ForeignKey(Toppings, on_delete=models.CASCADE)
+    topping_ingredients = models.CharField(max_length=40, null=False, blank=False)
+
+    def __str__(self):
+        return self.topping_ingredients
+
+class Flavors_Ingredients(models.Model):
+    flavor = models.ForeignKey(IceCreamFlavours, on_delete=models.CASCADE)
+    flavor_ingredients = models.CharField(max_length=40, null=False, blank=False)
+
+    def __str__(self):
+        return self.flavor_ingredients
+
+# class Orders(models.Model):
+#     name = models.CharField(max_length=50)
+#     email = models.EmailField()
+#     items = models.CharField(max_length=255)
+#     address = models.TextField()
+#     quantity = models.IntegerField()
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     phone_num = models.CharField(max_length=15)
+#     date_ordered = models.DateTimeField(auto_now_add=True)
+#
+#     def __str__(self):
+#         return f"Order {self.id} by {self.name}"
+
+
+
+
+# class Ingredients(models.Model):
+#     flavour = models.ForeignKey(IceCreamFlavours, on_delete=models.CASCADE)
+#     flavour_ingredients = models.TextField()
+#
+#     container = models.ForeignKey(Containers, on_delete=models.CASCADE)
+#     container_ingredients = models.TextField()
+#
+#     toppings = models.ForeignKey(Toppings, on_delete=models.CASCADE)
+#     toppings_ingredients = models.TextField()
+#
+#     def __str__(self):
+#         return f"Ingredients for {self.flavour}"
 
 
 class Size(models.Model):
